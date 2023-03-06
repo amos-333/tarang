efeccc356569e10d0abdf56ab3b99fa6ce233d22
sinan-marayi <sinanmarayi8@gmail.com>
2023-03-06 15:15:13 +0530

    overall

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,13 +8,18 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   addResult(result: any) {
-    return this.http.post<any>('http://localhost:8080/users/postResult', result);
+    return this.http.post<any>(
+      'http://localhost:8080/users/postResult',
+      result
+    );
   }
   getAllResult() {
     return this.http.get<any>('http://localhost:8080/users/getAllResult');
   }
-  deleteOneResult(id:any){
-    return this.http.post<any>('http://localhost:8080/users/deleteResult',id);
-
+  deleteOneResult(id: any) {
+    return this.http.post<any>('http://localhost:8080/users/deleteResult', id);
+  }
+  getOverall() {
+    return this.http.get<any>('http://localhost:8080/users/overall');
   }
 }
