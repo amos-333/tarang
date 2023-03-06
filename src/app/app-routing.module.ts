5a6dd38b8f293823cd12f3a7617ec5b2dfd1d645
amos-333 <over2amos333@gmail.com>
2023-03-06 12:51:19 +0530

    included rules page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RulesComponent } from './rules/rules.component';
 
 const routes: Routes = [
 {path:"" , component:HomeComponent},
+{path:"rules" , component:RulesComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PointTableComponent } from './point-table/point-table.component';
+import { RulesComponent } from './rules/rules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    PointTableComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
