85ab76e619c7326c23f0371cd0a937d5208e9e12
amos-333 <over2amos333@gmail.com>
2023-03-06 15:13:57 +0530

    point-table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PointTableComponent } from './point-table/point-table.component';
 import { RulesComponent } from './rules/rules.component';
 
 const routes: Routes = [
 {path:"" , component:HomeComponent},
 {path:"rules" , component:RulesComponent},
+{path:"point-table" , component:PointTableComponent},
 
 ];
 
