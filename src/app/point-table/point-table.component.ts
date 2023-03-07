9153eb0408e4bbcd79f3025bd4d9b1617e337400
sinan-marayi <sinanmarayi8@gmail.com>
2023-03-07 10:40:50 +0530

    overall

--- src/app/point-table/point-table.component.ts
+++ src/app/point-table/point-table.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-point-table',
   templateUrl: './point-table.component.html',
-  styleUrls: ['./point-table.component.css']
+  styleUrls: ['./point-table.component.css'],
 })
 export class PointTableComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  overall: any = [];
+active:string='eventwise'
+  constructor(private api: ApiService) {
+    this.getOverall();
   }
 
+  getOverall() {
+    this.api.getOverall().subscribe((res) => {
+      this.overall = res.data;
+      console.log(res);
+    });
+  }
+
+  ngOnInit(): void {}
 }
